f6c714c3028efb5be98f67c7d9b89293a1163568
Alex <Alexey-Idan-151@mail.ru>
2024-06-25 20:48:43 +0300

    Final Version

--- Guide/Guide.py
+++ Guide/Guide.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import messagebox, ttk
+from tkinter import ttk, filedialog, messagebox
 
 
 def work_with_phonebook():
@@ -56,7 +56,6 @@ def work_with_phonebook():
         return data
 
     def search_info():
-        patch
         results = []
         result_data = ' '.join(deleted_info_data()).split()
         print(result_data)
@@ -78,6 +77,42 @@ def work_with_phonebook():
         entry.grid(column=1, row=row, padx=10, pady=10)
         return entry
 
+    def copy_selected_line_to_file():
+        selected_index = listbox.curselection()
+        if not selected_index:
+            messagebox.showerror("Ошибка", "Выберите строку для копирования")
+            return
+
+        selected_line = listbox.get(selected_index[0])
+
+        dst_filename = filedialog.asksaveasfilename(
+            title="Выбрать файл для сохранения", filetypes=[("Текстовые файлы", "*.txt")]
+        )
+        if not dst_filename:
+            return
+
+        try:
+            with open(dst_filename, "w", encoding="utf-8") as dst_file:
+                dst_file.write(selected_line.strip())
+            messagebox.showinfo(
+                "Успех", "Выбранная строка успешно скопирована!")
+        except Exception as e:
+            messagebox.showerror(
+                "Ошибка", f"Ошибка при копировании строки:\n{e}")
+
+    def load_lines_to_listbox():
+        listbox.delete(0, END)  # Очищаем содержимое listbox
+
+        try:
+            with open(patch, "r", encoding="utf-8") as file:
+                lines = file.readlines()
+                for line in lines:
+                    # Добавляем строки в listbox
+                    listbox.insert(END, line.strip())
+        except Exception as e:
+            messagebox.showerror(
+                "Ошибка", f"Ошибка при загрузке данных из файла:\n{e}")
+
     window = Tk()
     window.title("Телефонный справочник")
 
@@ -85,9 +120,11 @@ def work_with_phonebook():
 
     tab_add = Frame(tab_control)
     tab_search = Frame(tab_control)
+    tab_copy = Frame(tab_control)
 
     tab_control.add(tab_add, text='Добавление')
     tab_control.add(tab_search, text='Поиск')
+    tab_control.add(tab_copy, text='Копировать')
     tab_control.pack(expand=1, fill='both')
 
     # Вкладка добавления
@@ -106,7 +143,7 @@ def work_with_phonebook():
     Label(tab_add, text="Введите ваш номер телефона").grid(
         column=0, row=3, padx=10, pady=10)
     entry_phone_number = create_entry_with_placeholder(
-        tab_add, "Номер телефона без +", 3)
+        tab_add, "Пишите номер без +", 3)
 
     btn_save = Button(tab_add, text="Сохранить", command=save_info)
     btn_save.grid(column=1, row=4, padx=10, pady=20)
@@ -129,13 +166,28 @@ def work_with_phonebook():
     Label(tab_search, text="Введите ваш номер телефона").grid(
         column=0, row=3, padx=10, pady=10)
     search_entry_phone_number = create_entry_with_placeholder(
-        tab_search, "Номер телефона без +", 3)
+        tab_search, "Пишите номер без +", 3)
 
     btn_search = Button(tab_search, text="Поиск", command=search_info)
     btn_search.grid(column=1, row=4, padx=10, pady=20)
 
     result_label = Label(tab_search, text="")
     result_label.grid(column=0, row=5, columnspan=2, padx=10, pady=10)
+
+    # Вкладка копирования
+    Label(tab_copy, text="Выберите строку для копирования:").grid(
+        column=0, row=0, padx=10, pady=10)
+
+    listbox = Listbox(tab_copy, selectmode=SINGLE)
+    listbox.grid(column=0, row=1, columnspan=2, padx=10, pady=10)
+
+    btn_load_lines = Button(
+        tab_copy, text="Загрузить строки", command=load_lines_to_listbox)
+    btn_load_lines.grid(column=0, row=2, padx=10, pady=10)
+
+    btn_copy_line = Button(
+        tab_copy, text="Копировать выбранную строку", command=copy_selected_line_to_file)
+    btn_copy_line.grid(column=0, row=3, columnspan=2, padx=10, pady=10)
 
     window.mainloop()
 
